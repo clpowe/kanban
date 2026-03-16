609143213c5e4c65ee389d8c5c609e48f2086eb3
clpowe <clpowe@gmail.com>
2026-03-16 10:05:37 -0400

    switched to hono and d1

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { Hono } from 'hono'
+import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1'
+import { users } from './schema'
+
+export type Env = {
+	Bindings: {
+		DB: DrizzleD1Database
+	}
+}
+
+const app = new Hono<Env>()
+
+app.get('/', (c) => {
+	return c.text('Hello Hono + D1 + Drizzle!')
+})
+
+app.get('/users', async (c) => {
+	const db = drizzle(c.env.DB)
+	const result = await db.select().from(users)
+	return c.json(result)
+})
+
+app.post('/users', async (c) => {
+	const body = await c.req.json()
+	const db = drizzle(c.env.DB)
+
+	const result = await db
+		.insert(users)
+		.values({
+			name: body.name,
+			email: body.email
+		})
+		.returning()
+
+	return c.json(result)
+})
+
+export default app
