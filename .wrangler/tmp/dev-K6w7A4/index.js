02638bf1f75d289a3545d59f589a29aaa0d26c87
clpowe <clpowe@gmail.com>
2026-06-06 23:40:45 -0400

    updated wrangler.json

--- .wrangler/tmp/dev-K6w7A4/index.js
+++ .wrangler/tmp/dev-K6w7A4/index.js
@@ -32,7 +32,7 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
   mod
 ));
 
-// .wrangler/tmp/bundle-Zh0W17/checked-fetch.js
+// .wrangler/tmp/bundle-72eNEf/checked-fetch.js
 function checkURL(request, init2) {
   const url2 = request instanceof URL ? request : new URL(
     (typeof request === "string" ? new Request(request, init2) : request).url
@@ -50,7 +50,7 @@ function checkURL(request, init2) {
 }
 var urls;
 var init_checked_fetch = __esm({
-  ".wrangler/tmp/bundle-Zh0W17/checked-fetch.js"() {
+  ".wrangler/tmp/bundle-72eNEf/checked-fetch.js"() {
     "use strict";
     urls = /* @__PURE__ */ new Set();
     __name(checkURL, "checkURL");
@@ -21675,11 +21675,11 @@ var init_kysely_adapter = __esm({
   }
 });
 
-// .wrangler/tmp/bundle-Zh0W17/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-72eNEf/middleware-loader.entry.ts
 init_checked_fetch();
 init_modules_watch_stub();
 
-// .wrangler/tmp/bundle-Zh0W17/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-72eNEf/middleware-insertion-facade.js
 init_checked_fetch();
 init_modules_watch_stub();
 
@@ -61638,7 +61638,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-Zh0W17/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-72eNEf/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -61672,7 +61672,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-Zh0W17/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-72eNEf/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
